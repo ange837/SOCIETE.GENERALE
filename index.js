becca9b8cb24cbde99cc146b5f2830f28783a9fa
ange837 <ange83783@gmail.com>
2026-02-19 14:12:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
   
-    //  identifiants
     if (username === "Weemans045" && password === "Camille03@") {
       window.location.href = "inter.html";
     } else {
@@ -13,10 +12,14 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
   });
   document.getElementById("loginForm").addEventListener("submit", (e) => {
   e.preventDefault();
+
   const button = document.querySelector(".btn-login");
   button.textContent = "Connexion...";
   button.disabled = true;
+
   setTimeout(() => {
     window.location.href = "index.html"; // ou dashboard, page comptes etc.
   }, 1800);
 });
+
+  
