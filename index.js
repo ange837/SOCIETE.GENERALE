38ae00be17681cf6bf1afb6406205ef3461f8276
ange837 <ange83783@gmail.com>
2026-02-19 13:30:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 document.getElementById("loginForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-  
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
-  
-    // Simulation : identifiants "admin" / "1234"
-    if (username === "Weemans045" && password === "Camille03@") {
-      window.location.href = "inter.html";
-    } else {
-      alert("Identifiants incorrects.");
-    }
-  });
-  document.getElementById("loginForm").addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const button = document.querySelector(".btn-login");
-  button.textContent = "Connexion...";
-  button.disabled = true;
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
 
-  setTimeout(() => {
-    window.location.href = "index.html"; // ou dashboard, page comptes etc.
-  }, 1800);
+  if (username === "Weemans045" && password === "Camille03@") {
+    const button = document.querySelector(".btn-login");
+    button.textContent = "Connexion...";
+    button.disabled = true;
+
+    setTimeout(() => {
+      window.location.href = "inter.html";
+    }, 1800);
+  } else {
+    alert("Identifiants incorrects.");
+  }
 });
-
   
